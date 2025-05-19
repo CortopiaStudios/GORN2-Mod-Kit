dc0e40abb7a0651d2b45c88fed297c8018b7e3ed
Johannes Westberg <wjohannes@hotmail.com>
2025-05-19 15:15:12 +0200

    Fixed bound value names on SpawnerParameter.cs

--- Assets/Cortopia/Scripts/Core/Spawn/SpawnerParameter.cs
+++ Assets/Cortopia/Scripts/Core/Spawn/SpawnerParameter.cs
@@ -4,12 +4,14 @@
 // and/or confidential. Reproduction or distribution, in whole or in part, is
 // forbidden except by express written permission of Cortopia Studios.
 
+using System;
 using Cortopia.Scripts.Reactivity;
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace Cortopia.Scripts.Core.Spawn
 {
-    public abstract class SpawnerParameter : MonoBehaviour
+    public abstract class SpawnerParameter : MonoBehaviour, INamedBindableReactiveOwner
     {
         public enum Direction
         {
@@ -20,6 +22,8 @@ namespace Cortopia.Scripts.Core.Spawn
 
         public string parameterName;
         public Direction direction;
+        public abstract Type ParameterType { get; }
+        public abstract string GetName(string propertyName);
     }
 
     public class SpawnerParameter<T> : SpawnerParameter
@@ -27,6 +31,14 @@ namespace Cortopia.Scripts.Core.Spawn
         [SerializeField]
         private BoundValue<T> boundValue;
 
+        [UsedImplicitly]
         public BoundValue<T> BoundValue => this.boundValue;
+
+        public override Type ParameterType => typeof(T);
+
+        public override string GetName(string propertyName)
+        {
+            return propertyName == nameof(this.BoundValue) ? this.parameterName : null;
+        }
     }
 }
